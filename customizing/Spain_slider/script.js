9565045b774edc8b8c9414550e95d482ebfc527d
Hoon <ehdgns730@gmail.com>
2024-07-16 21:08:18 +0900

    240716

--- customizing/Spain_slider/script.js
+++ customizing/Spain_slider/script.js
@@ -1,3 +1,29 @@
+// const thumnails = document.querySelectorAll("#thumnails > div");
+
+// thumnails.forEach((item, index) => {
+//     item.addEventListener("click", function() {
+//         thumnails.forEach((sibling) => {
+//             if(sibling !== item) {
+//                 sibling.classList.remove("active");
+//             }
+//             this.classList.add("active");
+//         });
+        
+//         const citys = document.querySelectorAll(".city");
+//         const backgourndImg = item.getAttribute("data-alt");
+//         const targetImg = document.getElementById(backgourndImg)
+//         const wrapper = document.querySelector(".wrapper");
+
+//         citys.forEach((city) => {
+//             city.classList.remove("active")
+//         });
+        
+//         citys[index].style = `background: url('./img/${backgourndImg}.jpg') center/cover no-repeat;`;
+//         citys[index].classList.add("active");
+//         wrapper.style = `left: ${index * -100}%`
+//     })
+// });
+
 const thumnails = document.querySelectorAll("#thumnails > div");
 
 thumnails.forEach((item, index) => {
@@ -11,16 +37,18 @@ thumnails.forEach((item, index) => {
         
         const citys = document.querySelectorAll(".city");
         const backgourndImg = item.getAttribute("data-alt");
+        const targetImg = document.getElementById(backgourndImg)
         const wrapper = document.querySelector(".wrapper");
-        
-        wrapper.style = `left: ${index * -100}%`
 
         citys.forEach((city) => {
             city.classList.remove("active")
         });
-        
-        citys[index].style = `background: url('./img/${backgourndImg}.jpg') center/cover no-repeat;`;
-        citys[index].classList.add("active");
 
+        wrapper.style = `left: ${index * -100}%`;
+
+        if (targetImg) {
+            targetImg.classList.add("active")
+            targetImg.style = `background: url('./img/${backgourndImg}.jpg') center/cover no-repeat;`;
+        }
     })
 });
