c046f31133651b5965d16c9bd28dc1f1a7d59335
Hoon <ehdgns730@gmail.com>
2024-07-21 23:54:44 +0900

    240721

--- customizing/RRL/script.js
+++ customizing/RRL/script.js
@@ -1,15 +1,19 @@
-const banner = document.querySelector(".banner");
-
+// header event
 window.addEventListener("scroll", () => {
     let windowScroll = window.scrollY;
+    const banner = document.querySelector(".banner");
+    const nav = document.querySelector("nav");
 
     if(windowScroll > 30) {
         banner.classList.add("active");
+        nav.classList.add("active");
     } else {
         banner.classList.remove("active");
+        nav.classList.remove("active");
     }
 });
 
+// main_txt event
 const title = "double rl&co.";
 const mainTitle = document.querySelector(".main_txt h3");
 
@@ -35,5 +39,19 @@ window.onload = function() {
     const mainPara = document.querySelector(".main_txt p");
     mainPara.classList.add("active");
 };
+
+// section event 
+
+
+window.addEventListener("scroll", function() {
+    let Scroll = this.scrollY;
+    const sections = document.querySelectorAll("section");
+
+    sections.forEach((section) => {
+        if(Scroll >= section.offsetHeight - this.window.innerHeight / 5) {
+            section.classList.add("active");
+        }
+    });
+})
 
 
