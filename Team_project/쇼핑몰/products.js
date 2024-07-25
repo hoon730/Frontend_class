d2cd0a7ae05d89a34ce0ec3f9eb57ed373db0fe1
hoon730 <ehdgns730@gmail.com>
2024-07-25 18:21:52 +0900

    240725

---
+++ Team_project/쇼핑몰/products.js
@@ -0,0 +1,41 @@
+const products = {
+  data: [
+    {
+      id: Date.now(),
+      name : "Cup",
+      price: 9000,
+      category: "life",
+      img :"./img/cup.jpg",
+    },
+    {
+      id: Date.now() + 1,
+      name : "Mouse",
+      price: 15000,
+      category: "it",
+      img :"./img/mouse.jpg",
+    },
+    {
+      id: Date.now() + 2,
+      name : "Keyboard",
+      price: 21000,
+      category: "it",
+      img :"./img/keyboard.jpg",
+    },
+    {
+      id: Date.now() + 3,
+      name : "Book",
+      price: 10000,
+      category: "book",
+      img :"./img/book.jpg",
+    },
+    {
+      id: Date.now() + 4,
+      name : "Pen",
+      price: 1500,
+      category: "book",
+      img :"./img/pen.jpg",
+    },
+  ]
+};
+
+export default products;
