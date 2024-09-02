805ad1e6bb1e73caf250c725e658fb84026f3032
hoon730 <ehdgns730@gmail.com>
2024-09-02 16:28:12 +0900

    merge with master

---
+++ 240826/04js/env.js
@@ -0,0 +1,3 @@
+const API_KEY = "015a25236f43f65695302da75b5ead5f";
+
+export default {API_KEY};
---
+++ 240902/04js/script.js
@@ -0,0 +1,23 @@
+const t1 = anime.timeline ({
+  easing: "linear",
+  duration: 1000,
+});
+
+t1.add({
+  targets: ".circle1",
+  translateX: 500,
+}).add({
+  targets: ".circle1",
+  translateY: 500,
+}).add({
+  targets: ".circle1",
+  translateX: 0,
+}).add({
+  targets: ".circle1",
+  translateY: 0,
+}).add({
+  targets: ".circle2",
+  scale: 1.5,
+  rotate: 360,
+  borderRadius: 0,
+})
