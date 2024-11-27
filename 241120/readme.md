3832bb0983834a48d6049e12c78522cf00cb0fa8
hoon730 <ehdgns730@gmail.com>
2024-11-27 16:37:26 +0900

    241117

--- 241120/readme.md
+++ 241120/readme.md
@@ -1,5 +1,18 @@
-/* RestFul API 방식을 사용한 경우! */
+/_ RestFul API 방식을 사용하지 않은 경우! _/
 
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/edit-user -> Edit User
+/delete-user -> Delete User
+
+/watch-video -> Watch Video
+/edit-video -> Edit Video
+/delete-video -> Delete Video
+
+/_ RestFul API 방식을 사용한 경우! _/
 
 / 글로벌 라우터
 / -> Home
@@ -17,30 +30,54 @@
 /video/watch -> Watch Video
 /video/edit -> Edit Video
 /video/delete -> Delete Video
+/video/comments -> Comment Video
 /video/comments/delete -> Delete Comment
 
-/* RestFul API 방식을 사용하지 않은 경우! */
+//
+Create
 
-Create 
 - video
-> user / ID / createdAt / source etc...
+  > user / ID / createdAt / source etc...
 
 Read
 
-Update
-> user / ID / createAt / source etc ...
+Upadte
+
+> user / ID / createdAt / source etc...
 
 Delete
+
 > All
 > 탈퇴 //
 
 schema => 윤곽 // 형태
-=> user로부터 어떤 데이터를 수집 => 우리가 원하는 형태로 관리, 보관, 제어
+=> user로부터 어떤 데이터를 수집 => 우리가 원하는 형태로 관리.보관.제어
 => data model => data modeling
 
 [
-  {
-    id: "Hoon"
-    source: ".mp4"
-  }
+{
+id: "david"
+source: ".mp4"
+.....
+},
+{
+id: "david"
+source: ".mp4"
+.....
+},
+{
+id: "david"
+source: ".mp4"
+.....
+},
+{
+id: "david"
+source: ".mp4"
+.....
+},
+{
+id: "david"
+source: ".mp4"
+.....
+},
 ]
--- 241120/src/controllers/videoController.js
+++ 241120/src/controllers/videoController.js
@@ -1,42 +1,27 @@
-import { formHashtags } from "../models/video";
-
-// const videos = [
-//   {
-//     id: 1,
-//     title: "First Video",
-//     createdAt: "2 minutes ago",
-//     views: 1,
-//     comment: 2,
-//     rating: 5,
-//   },
-//   {
-//     id: 2,
-//     title: "Second Video",
-//     createdAt: "2 minutes ago",
-//     views: 59,
-//     comment: 2,
-//     rating: 5,
-//   },
-//   {
-//     id: 3,
-//     title: "Third Video",
-//     createdAt: "2 minutes ago",
-//     views: 59,
-//     comment: 2,
-//     rating: 5,
-//   },
-// ];
 import Video from "../models/video";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ createdAt: "desc" });
     return res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     return res.render("server-error", { error });
   }
 };
-export const search = (req, res) => res.send("Search Video");
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
+};
+
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
@@ -54,8 +39,7 @@ export const getEdit = async (req, res) => {
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
-
-  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Edit : ${video.title}`, video });
 };
 
 export const postEdit = async (req, res) => {
@@ -63,12 +47,12 @@ export const postEdit = async (req, res) => {
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found" });
+    return res.render("404", { pageTitle: "Video not found." });
   }
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: formHashtags(hashtags),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -83,11 +67,10 @@ export const postUplold = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: formHashtags(hashtags),
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
-    console.error(error);
     return res.render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
@@ -95,7 +78,9 @@ export const postUplold = async (req, res) => {
   }
 };
 
-export const deleteVideo = (req, res) => {
-  console.log(req.params);
-  return res.send("Delete Video");
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  console.log(id);
+  return res.redirect("/");
 };
