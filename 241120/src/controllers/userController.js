52e31eed8228f6f562c06c01587bf6c65de60bbc
hoon730 <ehdgns730@gmail.com>
2024-11-29 16:31:47 +0900

    241129

--- 241120/src/controllers/userController.js
+++ 241120/src/controllers/userController.js
@@ -3,6 +3,31 @@ import User from "../models/user";
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
   const { email, username, password, name, location } = req.body;
+  const usernameExists = await User.exists({ username });
+  const pageTitle = "Join";
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username / email is already taken.",
+    })
+  }
+
+  if (usernameExists) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "This username is already taken.",
+    });
+  }
+
+  const emailExists = await User.exists({ email });
+  if (emailExists) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "This email is already taken.",
+    });
+  }
+
   await User.create({
     email,
     username,
